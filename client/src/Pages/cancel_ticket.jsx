edaf377cfedb518b2941e02beee59f07b125bd15
AL PJ <145345908+thesoftwareengr@users.noreply.github.com>
2023-11-28 14:14:39 +0800

    Cancel Reservation Component

--- client/src/Pages/cancel_ticket.jsx
+++ client/src/Pages/cancel_ticket.jsx
@@ -1,47 +1,72 @@
-import { Table } from "antd";
-import { useMovies } from "../Data/movie_data";
-import MenuTest from "../test/menu";
+import React from 'react';
+import { Table, Input, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+import { useMovies } from '../Data/movie_data';
 
-function CancelTicket(){
-    const columns = [
-        {
-          title: 'Title',
-          dataIndex: 'title',
-          key: 'title',
-        },
-        {
-          title: 'Date',
-          dataIndex: 'date',
-          key: 'date',
-        },
-        {
-          title: 'Cinema',
-          dataIndex: 'cinema',
-          key: 'cinema',
-        },
-        {
-          title: 'Date',
-          dataIndex: 'date',
-          key: 'date',
-        },
-        {
-          title: 'ID',
-          dataIndex: 'id',
-          key: 'id',
-        },
-        
-        {
-            title: 'Start',
-            dataIndex: 'start',
-            key: 'start',
-          },
-      ];
+const { Search } = Input;
 
-    return(
-        <div>
-            <h1>Cancel</h1>
-        </div>
-    )
-}
+const CancelTicket = () => {
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Cinema',
+      dataIndex: 'cinema',
+      key: 'cinema',
+    },
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Start',
+      dataIndex: 'start',
+      key: 'start',
+    },
+  ];
+
+  const onSearch = (value, _e, info) => console.log(info?.source, value);
+
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 20,
+        color: '#1677ff',
+      }}
+    />
+  );
+
+  return (
+    <div className= "cancel-component">
+      
+    <Space direction="vertical">
+      <Search
+        placeholder="input search text"
+        allowClear
+        enterButton="Search"
+        size="large"
+        onSearch={onSearch}
+      />
+
+      {/* <Search
+        placeholder="input search text"
+        enterButton="Search"
+        size="large"
+        suffix={suffix}
+        onSearch={onSearch}
+      /> */}
+    </Space>
+    </div>
+  );
+};
 
 export default CancelTicket;
