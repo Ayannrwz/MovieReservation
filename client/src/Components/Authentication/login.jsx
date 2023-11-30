ebdcb1dcc01d18f1069941533263ec76e042a92d
Simon Ynzon <ynzonsimon@gmail.com>
2023-11-30 14:48:08 +0800

    Designed login page

--- client/src/Components/Authentication/login.jsx
+++ client/src/Components/Authentication/login.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Form, Input, Button, Checkbox } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import '@fortawesome/fontawesome-free/css/all.css';
+import AuthenticationBackground from "../../assets/movies-authentication-background.jpg"
+
+import "../../Styles/signup.css"
 
 const NormalLoginForm = () => {
   const onFinish = (values) => {
@@ -10,11 +13,17 @@ const NormalLoginForm = () => {
   };
 
   return (
-    <div className="authen-container">
-      <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-      </div>
+    <div className="authentication-container">
+      <img 
+          src={AuthenticationBackground}
+          alt="Movies Background"
+          className="movies-authentication-background"
+      />
+      <div className="movies-authentication-background-overlay"/>
+      {/* <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
+      </div> */}
       <Form
         name="normal_login"
         className="login-form"
@@ -23,7 +32,7 @@ const NormalLoginForm = () => {
         }}
         onFinish={onFinish}
       >
-           <h3>Log in </h3>
+           <h3>LOG IN</h3>
         <div className="log">
      
         <Form.Item
@@ -59,7 +68,7 @@ const NormalLoginForm = () => {
         </Form.Item>
         <Form.Item>
           <Form.Item  name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
+            <Checkbox className="remember-me-text">Remember me</Checkbox>
           </Form.Item>
         </Form.Item>
 
