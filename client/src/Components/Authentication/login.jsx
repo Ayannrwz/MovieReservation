0a5b19c8ded05e0dc85fba63e7413c538fa454a8
AL PJ <145345908+thesoftwareengr@users.noreply.github.com>
2023-12-01 12:53:31 +0800

    finalize log in form

--- client/src/Components/Authentication/login.jsx
+++ client/src/Components/Authentication/login.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Form, Input, Button, Checkbox } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import '@fortawesome/fontawesome-free/css/all.css';
+
 import AuthenticationBackground from "../../assets/movies-authentication-background.jpg"
 
 import "../../Styles/signup.css"
@@ -39,11 +40,17 @@ const NormalLoginForm = () => {
     };
 
     return (
-        <div className="authen-container">
-            <div class="background">
+        <div className="authentication-container">
+          <img
+            src={AuthenticationBackground}
+            alt="Movies Background"
+            className="movies-authentication-background"
+          />
+          <div className="movies-authentication-background-overlay"/>
+            {/* <div class="background">
                 <div class="shape"></div>
                 <div class="shape"></div>
-            </div>
+            </div> */}
             <Form
                 name="normal_login"
                 className="login-form"
@@ -52,67 +59,60 @@ const NormalLoginForm = () => {
                 }}
                 onFinish={onFinish}
             >
-                <h3>Log in </h3>
-                <div className="log">
-                    <Form.Item
-                        name="username"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your Username",
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={
-                                <UserOutlined className="site-form-item-icon" />
-                            }
-                            placeholder="Username"
-                            autoComplete="off"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your Password",
-                            },
-                        ]}
-                    >
-                        <Input
-                            prefix={
-                                <LockOutlined className="site-form-item-icon" />
-                            }
-                            type="password"
-                            placeholder="Password"
-                            autoComplete="off"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Form.Item
-                            name="remember"
-                            valuePropName="checked"
-                            noStyle
-                        >
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-                    </Form.Item>
+            <h3>LOG IN</h3>
+            <div className="log">
+              {error && <div className="error-message">{error}</div>}
+          <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+            autoComplete="off"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+            autoComplete="off"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item  name="remember" valuePropName="checked" noStyle>
+            <Checkbox className="remember-me-text">Remember me</Checkbox>
+          </Form.Item>
+        </Form.Item>
 
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            className="login-form-button"
-                        >
-                            Log in
-                        </Button>
-                    </Form.Item>
-                </div>
-            </Form>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
         </div>
-    );
-};
+        
+      </Form>
+    </div>
+  );
+  };
 
-export default NormalLoginForm;
-
+  export default NormalLoginForm;
